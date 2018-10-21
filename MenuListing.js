064303fef981e3c1a9654c9451931b9f73b7bfbc
Suresh Jothilingam <sjothilingam@nymacbook57.home>
2018-10-21 18:12:19 -0400

    New Images, UI Fixes and Footer V1

--- MenuListing.js
+++ MenuListing.js
@@ -8,11 +8,41 @@ import {
   TouchableHighlight,
   FlatList,
   Text,
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
 
 import MenuDetails from './MenuDetails';
+import Footer from './Footer';
 
 class ListItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      minQuantity: 1,
+      quantity: 1,
+      maxQuantity: 10
+    };
+  }
+
+  _truncateHeaderText = (text) => {
+    return text.length > 16 ? (text.substr(0, 16)+'...') : text;
+  }
+  _incrementQuantity = () => {
+    if (this.state.quantity < this.state.maxQuantity){
+      this.setState({ quantity: this.state.quantity + 1 });
+    }
+  }
+
+  _decrementQuantity = () => {
+    if (this.state.quantity > this.state.minQuantity) {
+      this.setState({ quantity: this.state.quantity - 1 });
+    }
+  }
+  _addToCart = () => {
+
+  }
   _onPress = () => {
     this.props.onPressItem(this.props.index);
   }
@@ -21,60 +51,80 @@ class ListItem extends React.PureComponent {
     const item = this.props.item;
     
     return (
-     
-     <TouchableHighlight
-        onPress={this._onPress}
-        underlayColor='#dddddd' style={{flex:100,flexDirection:'row'}}>
-
-        <View style={styles.container}>
-          <View style={[{flexDirection:'row'}, styles.elementsContainer]}>
-            <Image source={require('./Resources/masaladosa.jpg')} style={styles.image}/>
-
-            <View style={{ flexDirection:'column', width: 200, height: 170}}> 
-
-                  <View style={{ flexDirection:'row', width: 300, height: 30, backgroundColor: '#91C738'}}>
-                         <Text>{item.Dishnm}</Text> 
-                    </View>
-                   <View style={{ flexDirection:'row', width: 300, height: 30}}>
-                       <Text>By {item.usernm} </Text> 
-                       <Image source={require('./Resources/callhomechef.png')} style={{height:20, width:20}}/>
-                  </View>
-                  
-                  
-                  <View style={{ flexDirection:'row'}}>
-                      <Text style={{flex: 1, flexWrap: 'wrap'}}> {item.Dishdesc}</Text> 
-                  </View>
-                  <View style={{ flexDirection:'row', width: 300, height: 30}}>
-                      <Image source={require('./Resources/Ellipse.png')} style={{height:20, width:20}}/>
-                 </View>
-                 <View style={{ flexDirection:'row', width: 300, height: 30}}>
-                      <Image source={require('./Resources/1083479.png')} style={{height:20, width:20}}/>
-                      <Text>8 Am - 10 PM</Text> 
-                 </View>
-                  
+      <View style={styles.container}>
+        <TouchableHighlight
+          onPress={this._onPress}
+          underlayColor='#dddddd' style={{ flex: 100, flexDirection: 'row' }}>
+          <View style={{flex:1, flexDirection:'row'}}>
+            {/* Left Image View */}
+            <View style={{flex:1, flexDirection:'column'}}>
+                <Image source={require('./Resources/masaladosa.jpg')} style={styles.image} />
             </View>
-
-          <View style={{ flexDirection:'column', width: 100, height: 100}}> 
-           <View style={{flexDirection:'row', width: 100, height: 30}}> 
-             <View style={{ backgroundColor:'#91C738'}}>
-                      <Text>Rs 50</Text> 
+            {/* Right Content View */}
+            <View style={{ flex:2, flexDirection: 'column'}}>
+              {/* Content Header */}
+              <View style={{ height: 30, flexDirection: 'row', backgroundColor: '#91C738', justifyContent: 'space-between', alignItems: 'center'}}>
+                <View stlye={{flex:1, flexDirection: 'column'}}>
+                  <Text style={{ color: '#FFFFFF', fontSize: 18, marginLeft: 5 }}>{this._truncateHeaderText(item.Dishnm)}</Text> 
+                </View>
+                <View stlyw={{ flex: 2, flexDirection: 'column' }}>
+                    <Text style={{ color: '#FFFFFF', fontSize: 18, marginRight: 5}}>Rs 50</Text> 
+                </View>
               </View>
-             
-              
+              {/* Content By and Time */}
+              <View style={{ flex: 2, flexDirection: 'row', justifyContent: 'space-between', marginTop: 10}}>
+                <View stlye={{flex:1, flexDirection: 'column'}}>
+                  <View style={{flex: 1, flexDirection: 'row'}}>
+                    <Text style={{marginRight: 2, marginLeft: 5}}>By {item.usernm} </Text> 
+                    <Image source={require('./Resources/callhomechef.png')} style={{ height: 20, width: 20}} />
+                  </View>
+                </View>
+                <View stlye={{ flex: 2, flexDirection: 'column' }}>
+                  <View style={{flex: 1, flexDirection: 'row'}}>
+                    <Image source={require('./Resources/generic_time.png')} style={{ height: 20, width: 20, marginRight: 5, marginLeft: 5}} />
+                    <Text style={{marginRight: 5}}>8 Am - 10 PM</Text> 
+                  </View>
+                </View>
+              </View>
+              {/* Content Description */}
+              <View style={{ flex: 2, flexDirection: 'row'}}>
+                <Text style={{marginLeft: 5}}>{item.Dishdesc}</Text>
+              </View>
+              {/* Content Type Icon */}
+              <View style={{ flex: 3, flexDirection: 'row' }}>
+                <Image source={require('./Resources/veg_tag.png')} style={{ height: 20, width: 20, marginLeft: 5}} />
+              </View>
+            </View>
           </View>
+        </TouchableHighlight>
+        {/*Quantity and Button*/}
+        <View style={{ flex: 2, flexDirection: 'row', alignItems: 'center'}}>
+          {/*Quantity Select*/}
+          <View style={{ flex: 1, flexDirection: 'column' }}>
+            <View style={{ width: 120, flexDirection: 'row', marginTop: 5, padding: 5, backgroundColor: '#91C738', justifyContent: 'center', borderRadius: 100, marginLeft: 5 }}>
+              <TouchableOpacity style={{ width: 40, textAlign: 'center'}} onPress={this._incrementQuantity}>
+                <Text style={{ textAlign: 'center', color: '#FFFFFF', fontWeight: 'bold', fontSize: 20}}>+</Text>
+              </TouchableOpacity>
+              <Text style={{ width: 40, textAlign: 'center', color: '#FFFFFF', fontWeight: 'bold', fontSize: 22}}>{this.state.quantity}</Text>
+              <TouchableOpacity style={{ width: 40, textAlign: 'center' }} onPress={this._decrementQuantity}>
+                <Text style={{ textAlign: 'center', color: '#FFFFFF', fontWeight: 'bold', fontSize: 20}}>-</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          {/*Add Button*/}
+          <View stlye={{ flex: 2, flexDirection: 'column' }}>
+            <TouchableOpacity style={{ width: 100, padding: 5, backgroundColor: '#91C738', marginTop: 5, alignItems: 'center', borderRadius: 100, marginRight: 5, justifyContent: 'center' }} onPress={this._addToCart}>
+              <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 20, textAlign: 'center' }}> Add</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
-
- </TouchableHighlight>
-      
     );
   }
 }
 
 //Next, add the component:
-
-export default class MoviesPlaying extends Component<{}> {
+export default class MoviesPlaying extends Component {
  _keyExtractor = (item, index) => index.toString();
 
   _renderItem = ({item, index}) => (
@@ -93,124 +143,28 @@ export default class MoviesPlaying extends Component<{}> {
       component: MenuDetails,
       passProps: {movie: this.props.listings[index]}
       });
-   
   };
  
   render() {
     return (
-      <FlatList
-        data={this.props.listings}
-        keyExtractor={this._keyExtractor}
-        renderItem={this._renderItem}
-      />
+      <View style={{flex: 1}}>
+        <ScrollView>
+          <FlatList data={this.props.listings} keyExtractor={this._keyExtractor} renderItem={this._renderItem} />
+        </ScrollView>
+        <Footer activePage='MenuListing'/>
+      </View>
     );
   }
 
 }
 
 const styles = StyleSheet.create({
-
-  testContainer:{
-    flex:1,
-  },
-
-  navBar:{
-    height:55,
-    backgroundColor:'white',
-    elevation:3,
-
-  },
-
-  tabBar: {
-    backgroundColor:'red',
-    height: 60
-  },
-
-  newcontainer: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start'
-  },
-  item :{
-    flex: 0.25,
-    // width: 150, //using fixed item width instead of flex: 0.5 works
-    height: 100,
-    // flexGrow: 1,
-    // flexShrink: 0,
-    padding: 10,
-  },
-
-  description: {
-    marginBottom: 20,
-    fontSize: 24,
-    textAlign: 'center',
-    color: '#656565'
-  },
-  containerold: {
-    flexDirection: 'row',
-    //flex:1,
-   // flexWrap: 'wrap',
-    padding: 5,
-    marginTop: 2,
-    width:'50%',
-    height: '25%'
-  },
- thumb: {
-    width: 80,
-    height: 100,
-    marginLeft: 5,
-    marginRight: 10
-  },
-  textContainer: {
-    flex: 1,
-  },
-  separator: {
-    height: 1,
-    backgroundColor: '#dddddd'
-  },
-  textdescriptor: {
-    fontSize: 12,
-    fontWeight: 'bold',
-    color: '#000000'
-  },
-  title: {
-    fontSize: 18,
-    color: '#F9F9F9',
-    fontFamily: 'Arial',
-    backgroundColor: '#91C738',
-    textAlign: 'left',
-    width: '130%'
-  },
-  title2: {
-    fontSize: 18,
-    textAlign: 'right'
-  },
-  rowContainer: {
-    flexDirection: 'row',
-    padding: 5
-  },
-
   container: {
-    marginTop: 15,
+    marginTop: 20,
     flex: 1
   },
-  headerStyle: {
-    fontSize: 24,
-    textAlign: 'center',
-    fontWeight: '100',
-    marginBottom: 24
+  image: {
+    width: undefined,
+    height: 150
   },
-  elementsContainer: {
-    flex: 1,
-    backgroundColor: '#ecf5fd',
-    marginLeft: 4,
-    marginRight: 4,
-    marginBottom: 1
-  },
-   image: {
-  width: 130,
-  height: 150
-},
-  
 });
