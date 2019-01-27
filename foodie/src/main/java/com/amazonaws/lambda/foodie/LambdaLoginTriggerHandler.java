c00d3dacf935d430083b74ace65f111bd1e31592
Meena <meenas@gmail.com>
2019-01-26 22:48:01 -0500

    added new code

--- foodie/src/main/java/com/amazonaws/lambda/foodie/LambdaLoginTriggerHandler.java
+++ foodie/src/main/java/com/amazonaws/lambda/foodie/LambdaLoginTriggerHandler.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,6 +15,7 @@ import java.sql.Statement;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -24,6 +26,8 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.amazonaws.services.lambda.runtime.events.CognitoEvent;
 import com.engine.foodie.OrderManagement;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class LambdaLoginTriggerHandler implements RequestStreamHandler{
 
@@ -54,14 +58,19 @@ public class LambdaLoginTriggerHandler implements RequestStreamHandler{
 	 * @see com.amazonaws.services.lambda.runtime.RequestStreamHandler#handleRequest(java.io.InputStream, java.io.OutputStream, com.amazonaws.services.lambda.runtime.Context)
 	 */
 	
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
 	
 	
     @SuppressWarnings("unchecked")
 	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
 
-        
+    	//InputStream stream = input;
+        //JsonNode eventNode = OBJECT_MAPPER.readTree(input);  // root node
+
         
     	LambdaLogger logger = context.getLogger();
+    	logger.log("New Login code eee");
     	
     	InputStreamReader isr = new InputStreamReader(input, Charset.forName("UTF-8"));
     	BufferedReader br = new BufferedReader(isr);
@@ -90,12 +99,62 @@ public class LambdaLoginTriggerHandler implements RequestStreamHandler{
         }
 
         JSONObject inObj =  (JSONObject)objIn;
+        logger.log(inObj.toJSONString());
         
-        System.out.println(inObj.toJSONString());
+        
+        
+        String user_nm = (String) inObj.get("userName");
+        logger.log("user_nm " + user_nm);
+
+        
+        int userid = getUserId(100000,1000000) ;
+        
+        
+        String email = (String) ((JSONObject) ((JSONObject)inObj.get("request")).get("userAttributes")).get("email");
+        logger.log("email  " + email);
+
+        
+/*        JSONArray arr = ((Object) inObj).getJSONArray("request");
+        for (int i = 0; i < arr.length(); i++)
+        {
+            String post_id = arr.getJSONObject(i).getString("post_id");
+            ......
+        }
+
+        Map req_params = ((Map)inObj.get()); 
+        
+        // iterating address Map 
+        Iterator<Map.Entry> itr1 = req_params.entrySet().iterator(); 
+        while (itr1.hasNext()) { 
+            Map.Entry pair = itr1.next(); 
+            logger.log(pair.getKey() + " : " + pair.getValue()); 
+        }
+        
+  */  	
+    	/*
     	
+    	//---- top-level items in event like userName & request
+        String reqPoolId = eventNode.path("userPoolId").asText();
+        String user_nm = eventNode.path("userName").asText();  // camel-case
+        String userid = eventNode.path("userid").asText();  // camel-case
+        
+        
+//        JsonNode callerContextNode = eventNode.path("callerContext");
+        JsonNode requestNode = eventNode.path("request");
+ 
+        //---- items under event.request like userAttrs
+        JsonNode userAttrsNode = requestNode.path("userAttributes");
+ 
+        //---- items under event.request.userAttributes like email
+        String email = userAttrsNode.path("email").asText();
+        String phone = userAttrsNode.path("phone").asText();
         // Get time from DB server
     	logger.log("Invoked Foodie LOGIN  " );
-    	
+    	*/
+
+        logger.log(" User info  " + userid + " " + user_nm + " " + email);
+        
+        
     	try {
           String url = "jdbc:mysql://foodie.cl8mn8l8ahnm.us-east-1.rds.amazonaws.com:3306";
           String username = "Lokmeer";
@@ -105,13 +164,11 @@ public class LambdaLoginTriggerHandler implements RequestStreamHandler{
           Statement stmt = conn.createStatement();
           //ResultSet resultSet = stmt.executeQuery("SELECT NOW()");
           stmt.executeUpdate("INSERT INTO FoodieDB.tb_User "
-          		+ "(User_id, Community_id,  Building_id, Flat_id, Diet_type_id, User_nm, Email_addr, Phone_nbr, Encrypted_pwd, Seller_flg) " 
-  + "VALUES (1000006, 1, 5, 36, 1, \"Kalai\", \"lokeya@gmail.com\", \"8778454872\", \"kdhskjfhskdjfhskhfskjfhsdsdkjfskldfjdslf\", 0)");
+          		+ "(User_id, Community_id,  Building_id, Flat_id, Diet_type_id, User_nm, Email_addr, Phone_nbr, Encrypted_pwd, Seller_flg) "  
+  + "VALUES (" + userid  +  ", 1, 5, 36, 1, \" " +  user_nm  + "\", \" "+ email + " \", \"8778454872\", \"kdhskjfhskdjfhskhfskjfhsdsdkjfskldfjdslf\", 0)");
 
          /**********Order Management Calls*************/
           
-          
-                     
            
         } catch (Exception e) {
           e.printStackTrace();
@@ -122,8 +179,26 @@ public class LambdaLoginTriggerHandler implements RequestStreamHandler{
         logger.log("Successfully executed login query.  Result for LokMeer Project " );
         
         
-        return ;
+        //output.write(in.getBytes()); ;
+        
+        //OBJECT_MAPPER.writeValue(output,eventNode);
+    
+     // Success Condition, send back same JSON
+        try (Writer w = new OutputStreamWriter(output, "UTF-8")) {
+        w.write(inObj.toString());
+        }
+    
     }
     
+    
+    public int getUserId(int maximum, int minimum)
+    {
+    	Random rn = new Random();
+    	int n = maximum - minimum + 1;
+    	int i = rn.nextInt() % n;
+    	int randomNum =  minimum + i;
+    	
+    	return randomNum;
+    }
    
 }
